60fe49204d46999e7a5af22e1ae1d3fedde9ffd4
Minhnghia100402 <nghialeefcb@gmail.com>
2024-01-25 11:33:45 +0700

    Employees Router

--- framework/expressJS/connectMysql/src/routers/employessRouter.js
+++ framework/expressJS/connectMysql/src/routers/employessRouter.js
@@ -2,7 +2,10 @@ const controller = require('./../controllers/employeeController');
 var express = require('express');
 var router = express();
 
-router.get('/allemployees', controller.allEmployees);
-router.get('/alldepartments', controller.allDepartments);
+router.get('/allemployees', controller.gettAllEmployees);
+router.get('/alldepartments', controller.getAllDepartments);
 router.post('/createemployee', controller.createEmployee);
+router.post('/updateemployee', controller.updateEmployee);
+router.post('/deleteemployee', controller.deleteEmployee);
+router.post('/getoneemployee', controller.getOneEmployee);
 module.exports = router;
