f5cd1eb8a84227c5c81462e5bf20336aab64deb5
Minhnghia100402 <nghialeefcb@gmail.com>
2024-01-25 11:33:06 +0700

    Employees Controllers

--- framework/expressJS/connectMysql/src/controllers/employeeController.js
+++ framework/expressJS/connectMysql/src/controllers/employeeController.js
@@ -1,9 +1,10 @@
 var conn = require('./../index').conn;
+const e = require('express');
 const employees = require('./../models/employeesModels');
 const employeesModels = new employees(conn);
 
 
-const allEmployees = function (req, res) {
+const gettAllEmployees = function (req, res) {
     var sql = 'SELECT * FROM Employees;';
     conn.query(sql, function (err, data, fields) {
         if (err) {
@@ -15,7 +16,7 @@ const allEmployees = function (req, res) {
         }
     }); 
 }
-const allDepartments = function(req, res) {
+const getAllDepartments = function(req, res) {
     var sql = 'SELECT * FROM Departments';
     conn.query(sql, function(err,data, fields) {
         if (err) throw err;
@@ -43,8 +44,72 @@ const createEmployee = (req,res) => {
         });
     }
 }
+
+
+const updateEmployee = (req, res) => {
+    const data = req.body;
+    if (!data.employee_id || !data.first_name || !data.last_name || !data.email || !data.department_id)
+    {
+        return res.status(404).json({error:' Not found '})
+
+    }else{
+        employeesModels.updateEmployee(data,(err,data)=>{
+            if (err){
+                res.status(500).json({error:'Email already exist'});
+            }else{
+                res.status(200).json({success:'Updated Successfully'});
+            }
+        })
+    }
+}
+
+const deleteEmployee = (req,res) => {
+    const data = req.body;
+    console.log(data);
+    if (!data.employee_id || !data.first_name)
+    {
+        return res.status(404).json({message:"Not found id"});
+    }else{
+        employeesModels.deleteEmployee(data,(err, data) =>{
+            if(err) 
+            {
+                return res.status(500).json({message: "Not Found id in database"});
+            }else{
+                res.status(200).json({message:"Delete successful"});
+            }
+        })
+    }
+
+}
+
+const getOneEmployee = (req, res) => {
+    const data = req.body;
+    if(!data.employee_id)
+    {
+        return res.status(500).json({message:'Not Found ID'});
+    }else{
+        employeesModels.getOneEmployee(data,(err, result) => {
+            if(err) 
+            {
+                return res.json({server:'Not Found ID'});
+            }else{
+                // console.log(typeof(result));
+                // return res.json({message:result[0]});
+                if(result && result.length > 0)
+                {
+                    return res.json({message:result[0]});
+                }else{
+                    return res.json({message:'ID is not available'});
+                }
+            }
+        });
+    }
+}
 module.exports = {
-    allDepartments,
-    allEmployees,
+    getAllDepartments,
+    gettAllEmployees,
     createEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getOneEmployee,
 }
